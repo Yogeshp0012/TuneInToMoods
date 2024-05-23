eba444994999d7831ce25a523a7ac986122399b4
Yogeshp0012 <yogeshp0012@gmail.com>
2024-05-23 22:05:10 +0530

    Add Readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TodosComponent } from './todos/todos.component';
+import { RouterModule } from '@angular/router';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
+import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
 
 Amplify.configure(outputs);
 
@@ -11,8 +11,13 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterModule,AmplifyAuthenticatorModule],
 })
 export class AppComponent {
-  title = 'amplify-angular-template';
+  title = 'TuneInToMoods';
+
+
+constructor(public authenticator: AuthenticatorService) {
+  Amplify.configure(outputs);
+}
 }
